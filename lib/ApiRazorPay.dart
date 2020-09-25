f6de22fdeea21bc9dcda3df1911c89e012e78dd0
harshitparkar <harshit.parkar@sakec.ac.in>
2020-09-25 23:25:23 +0530

    RazorPay Auto Capture(OrderAPI) Done

--- lib/ApiRazorPay.dart
+++ lib/ApiRazorPay.dart
@@ -1,13 +1,47 @@
+import 'dart:convert';
+
+import 'package:AOL_localfeedback/OrderApi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:AOL_localfeedback/ProjectPage.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:fluttertoast/fluttertoast.dart';
 
 class ApiRazorPay extends StatefulWidget {
   @override
   _ApiRazorPayState createState() => _ApiRazorPayState();
+}
+String idd;
+Future<OrderApi> createOrder(String amount,String id,{String currency = "INR", String receipt = "receipt"}) async {
+  final String apiUrls = "https://api.razorpay.com/v1/orders";
+//  Map<String,String> myHeaders = new Map<String,String>();
+  Map<String, String> myHeaders = {
+//    "Content-Type": "application/json",
+    "Authorization":
+        "Basic cnpwX3Rlc3Rfc1dVUnFSUXlmUXpZbUg6QmhkM1pEbmx6elphV2R6UXJiNkpWWlRQ",
+  };
+
+  final response = await http.post(apiUrls, headers: myHeaders, body: {
+    "amount": amount.toString(),
+    "currency": currency,
+    "receipt": receipt,
+  });
+  print("################################"+response.toString());
+  final String responseString = response.body;
+
+  id = responseString.substring(7, 27);
+  print("respose is                                                "+responseString );
+  Map responsejson = jsonDecode(response.body.toString());
+  idd = responsejson["id"];
+  //debugPrint();
+
+//    for(int i=0;i<=2;i++){
+//      print(responseString['id']);
+//    }
+//    print(responseString);
+  return orderApiFromJson(responseString);
 }
 
 class _ApiRazorPayState extends State<ApiRazorPay> {
@@ -26,7 +60,17 @@ class _ApiRazorPayState extends State<ApiRazorPay> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              RaisedButton(onPressed: openCheckout, child: Text('Open'))
+              RaisedButton(
+                onPressed: () async {
+                  final OrderApi order = await createOrder('100000',idd);
+                  print(idd);
+//                  print("order is      "+order.toString());
+//                  print("xnw1ixi22nxi2" + order.toString());
+
+                  openCheckout();
+                },
+                child: Text('Open'),
+              )
             ],
           ),
         ),
@@ -51,12 +95,11 @@ class _ApiRazorPayState extends State<ApiRazorPay> {
   }
 
   void openCheckout() async {
+//    String _id=id.substring(7,27);
     var options = {
-    "razorpay_payment_id": "pay_29QQoUBi66xm2f",
-    "razorpay_order_id": "order_9A33XWu170gUtm",
-    "razorpay_signature": "9ef4dffbfd84f1318f6739a3ce19f9d85851857ae648f114332d8401e0949a3d",
-      'key': 'rzp_test_gOZLxZMO8J2ap7',
-      'amount':1000000,
+      "order_id": idd,
+      'key': 'rzp_test_sWURqRQyfQzYmH',
+      'amount': 100000,
       'name': 'Art Of Living',
       'description': 'Watershed Management Project',
       'prefill': {'contact': '', 'email': ''},
@@ -72,28 +115,36 @@ class _ApiRazorPayState extends State<ApiRazorPay> {
     }
   }
 
-  String success='';
+  String success = '';
 
   void _handlePaymentSuccess(PaymentSuccessResponse response) {
-
     Fluttertoast.showToast(
         msg: "SUCCESS: " + response.paymentId, timeInSecForIosWeb: 4);
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> ProjectPage()));
-    success=response.paymentId.toString();
-    print("                                         Payment ID                                                      "+success);
-    print("                                         OrderID                                                           "+response.orderId.toString());
-    print("                                        Signature                                                           "+response.signature.toString());
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => ProjectPage()));
+    success = response.paymentId.toString();
+    print(
+        "                                         Payment ID                                                      " +
+            success);
+    print(
+        "                                         OrderID                                                           " +
+            response.orderId.toString());
+    print(
+        "                                        Signature                                                           " +
+            response.signature.toString());
   }
 
   void _handlePaymentError(PaymentFailureResponse response) {
-
     Fluttertoast.showToast(
         msg: "ERROR: " + response.code.toString() + " - " + response.message,
         timeInSecForIosWeb: 4);
-    success=response.code.toString();
-    print("                                                                                               "+success);
-    debugPrint("******f*********************************************************"+response.message.toString());
-
+    success = response.code.toString();
+    print(
+        "                                                                                               " +
+            success);
+    debugPrint(
+        "******f*********************************************************" +
+            response.message.toString());
   }
 
   void _handleExternalWallet(ExternalWalletResponse response) {
